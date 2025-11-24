7b3ecdc9f837594719e6655d89972f03a76e4cf3
Talita Morales <talitamorales954@gmail.com>
2025-11-24 20:33:27 -0300

    removing the animation

--- app/src/main/java/com/talitamorales/composememory/gamelogic/MemoryCard.kt
+++ app/src/main/java/com/talitamorales/composememory/gamelogic/MemoryCard.kt
@@ -1,9 +1,6 @@
 package com.talitamorales.composememory.gamelogic
 
 
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,7 +11,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,7 +24,6 @@ import androidx.compose.ui.unit.dp
 import com.talitamorales.composememory.R
 import com.talitamorales.composememory.ui.theme.PinkCardFaceUp
 import com.talitamorales.composememory.ui.theme.PurpleCardFaceDown
-import kotlinx.coroutines.delay
 
 
 @Composable
@@ -36,21 +31,6 @@ fun MemoryCard(card: Card, isMemorizing: Boolean, onClick: () -> Unit) {
 
     var isFlipped by remember(card.id) { mutableStateOf(card.isFaceUp || card.isMatched) }
     val targetFlip = card.isFaceUp || card.isMatched
-
-    // Animação suave de 0° a 180°
-    val rotation by animateFloatAsState(
-        targetValue = if (targetFlip) 180f else 0f,
-        animationSpec = tween(600, easing = FastOutSlowInEasing),
-        label = "flip"
-    )
-
-    // Atualiza o estado interno apenas quando a animação termina
-    LaunchedEffect(targetFlip) {
-        if (!targetFlip) {
-            delay(300) // espera metade da animação para "esconder" a frente
-        }
-        isFlipped = targetFlip
-    }
 
     Card(
         modifier = Modifier
@@ -63,32 +43,26 @@ fun MemoryCard(card: Card, isMemorizing: Boolean, onClick: () -> Unit) {
         Box(
            modifier = Modifier
                .fillMaxSize()
-               .graphicsLayer {
-                   rotationY = rotation
-                   cameraDistance = 12f * density // essencial para 3D
-               }
                .background(
-                   color = if (rotation <= 90f) PurpleCardFaceDown else PinkCardFaceUp,
+                   color = if (card.isFaceUp || card.isMatched) PinkCardFaceUp else PurpleCardFaceDown,
                    shape = RoundedCornerShape(12.dp)
                ),
             contentAlignment = Alignment.Center
         ) {
-            if (rotation <= 90f && (card.isFaceUp || card.isMatched)) {
+            if (card.isFaceUp || card.isMatched) {
                 Image(
                     painter = painterResource(id = card.imageRes),
                     contentDescription = "Animal",
                     modifier = Modifier.size(85.dp)
-                        .graphicsLayer { rotationY = 0f }
                 )
-            }
-            if (rotation >= 90f && !(card.isFaceUp || card.isMatched)) {
+            } else {
                 Image(
                     painter = painterResource(id = R.drawable.card_back),
                     contentDescription = "Card Back",
                     modifier = Modifier.size(85.dp)
-                        .graphicsLayer { rotationY = 80f }
                 )
             }
+
         }
     }
 }
